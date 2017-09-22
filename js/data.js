1aab4016f58090999b2fc23655f7905936907b3a
Steven Dao <iamstevendao@users.noreply.github.com>
2017-09-23 00:30:42 +1000

    projects to angular

--- js/data.js
+++ js/data.js
@@ -3,10 +3,17 @@
   var App = angular.module('data', []);
 
   App.controller('DataController', function DataController($scope, $http) {
+
+    //timeline
     $http.get('json/timeline.json')
       .then(function (res) {
         $scope.timeline = res.data;
       });
-  });
 
+    //projects
+    $http.get('json/projects.json')
+      .then(function (res) {
+        $scope.projects = res.data;
+      });
+  });
 })(window.angular);
