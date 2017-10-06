5f83ef555eea374289cca0c7a18e5d66cc8ae480
Steven Dao <iamstevendao@users.noreply.github.com>
2017-10-06 18:40:46 +1100

    reformat using standardjs

--- js/angular-smooth-scroll.js
+++ js/angular-smooth-scroll.js
@@ -15,10 +15,9 @@
  */
 
 (function () {
-  'use strict';
+  'use strict'
 
-  var module = angular.module('smoothScroll', []);
-
+  var module = angular.module('smoothScroll', [])
 
 	/**
 	 * Smooth scrolls the window/div to the provided element.
@@ -27,7 +26,7 @@
 	 * 	Added new option - containerId to account for scrolling within a DIV
 	 */
   var smoothScroll = function (element, options) {
-    options = options || {};
+    options = options || {}
 
     // Options
     var duration = options.duration || 800,
@@ -36,22 +35,22 @@
       callbackBefore = options.callbackBefore || function () { },
       callbackAfter = options.callbackAfter || function () { },
       container = document.getElementById(options.containerId) || null,
-      containerPresent = (container != undefined && container != null);
+      containerPresent = (container != undefined && container != null)
 
 		/**
 		 * Retrieve current location
 		 */
     var getScrollLocation = function () {
       if (containerPresent) {
-        return container.scrollTop;
+        return container.scrollTop
       } else {
         if (window.pageYOffset) {
-          return window.pageYOffset;
+          return window.pageYOffset
         } else {
-          return document.documentElement.scrollTop;
+          return document.documentElement.scrollTop
         }
       }
-    };
+    }
 
 		/**
 		 * Calculate easing pattern.
@@ -62,36 +61,36 @@
 		 */
     var getEasingPattern = function (type, time) {
       switch (type) {
-        case 'easeInQuad': return time * time; // accelerating from zero velocity
-        case 'easeOutQuad': return time * (2 - time); // decelerating to zero velocity
-        case 'easeInOutQuad': return time < 0.5 ? 2 * time * time : -1 + (4 - 2 * time) * time; // acceleration until halfway, then deceleration
-        case 'easeInCubic': return time * time * time; // accelerating from zero velocity
-        case 'easeOutCubic': return (--time) * time * time + 1; // decelerating to zero velocity
-        case 'easeInOutCubic': return time < 0.5 ? 4 * time * time * time : (time - 1) * (2 * time - 2) * (2 * time - 2) + 1; // acceleration until halfway, then deceleration
-        case 'easeInQuart': return time * time * time * time; // accelerating from zero velocity
-        case 'easeOutQuart': return 1 - (--time) * time * time * time; // decelerating to zero velocity
-        case 'easeInOutQuart': return time < 0.5 ? 8 * time * time * time * time : 1 - 8 * (--time) * time * time * time; // acceleration until halfway, then deceleration
-        case 'easeInQuint': return time * time * time * time * time; // accelerating from zero velocity
-        case 'easeOutQuint': return 1 + (--time) * time * time * time * time; // decelerating to zero velocity
-        case 'easeInOutQuint': return time < 0.5 ? 16 * time * time * time * time * time : 1 + 16 * (--time) * time * time * time * time; // acceleration until halfway, then deceleration
-        default: return time;
+        case 'easeInQuad': return time * time // accelerating from zero velocity
+        case 'easeOutQuad': return time * (2 - time) // decelerating to zero velocity
+        case 'easeInOutQuad': return time < 0.5 ? 2 * time * time : -1 + (4 - 2 * time) * time // acceleration until halfway, then deceleration
+        case 'easeInCubic': return time * time * time // accelerating from zero velocity
+        case 'easeOutCubic': return (--time) * time * time + 1 // decelerating to zero velocity
+        case 'easeInOutCubic': return time < 0.5 ? 4 * time * time * time : (time - 1) * (2 * time - 2) * (2 * time - 2) + 1 // acceleration until halfway, then deceleration
+        case 'easeInQuart': return time * time * time * time // accelerating from zero velocity
+        case 'easeOutQuart': return 1 - (--time) * time * time * time // decelerating to zero velocity
+        case 'easeInOutQuart': return time < 0.5 ? 8 * time * time * time * time : 1 - 8 * (--time) * time * time * time // acceleration until halfway, then deceleration
+        case 'easeInQuint': return time * time * time * time * time // accelerating from zero velocity
+        case 'easeOutQuint': return 1 + (--time) * time * time * time * time // decelerating to zero velocity
+        case 'easeInOutQuint': return time < 0.5 ? 16 * time * time * time * time * time : 1 + 16 * (--time) * time * time * time * time // acceleration until halfway, then deceleration
+        default: return time
       }
-    };
+    }
 
 		/**
 		 * Calculate how far to scroll
 		 */
     var getEndLocation = function (element) {
-      var location = 0;
+      var location = 0
       if (element.offsetParent) {
         do {
-          location += element.offsetTop;
-          element = element.offsetParent;
-        } while (element);
+          location += element.offsetTop
+          element = element.offsetParent
+        } while (element)
       }
-      location = Math.max(location - offset, 0);
-      return location;
-    };
+      location = Math.max(location - offset, 0)
+      return location
+    }
 
     // Initialize the whole thing
     setTimeout(function () {
@@ -103,19 +102,19 @@
         percentage,
         position,
         scrollHeight,
-        internalHeight;
+        internalHeight
 
 			/**
 			 * Stop the scrolling animation when the anchor is reached (or at the top/bottom of the page)
 			 */
       var stopAnimation = function () {
-        currentLocation = getScrollLocation();
+        currentLocation = getScrollLocation()
         if (containerPresent) {
-          scrollHeight = container.scrollHeight;
-          internalHeight = container.clientHeight + currentLocation;
+          scrollHeight = container.scrollHeight
+          internalHeight = container.clientHeight + currentLocation
         } else {
-          scrollHeight = document.body.scrollheight;
-          internalHeight = window.innerHeight + currentLocation;
+          scrollHeight = document.body.scrollheight
+          internalHeight = window.innerHeight + currentLocation
         }
 
         if (
@@ -129,39 +128,37 @@
             internalHeight >= scrollHeight
           )
         ) { // stop
-          clearInterval(runAnimation);
-          callbackAfter(element);
+          clearInterval(runAnimation)
+          callbackAfter(element)
         }
-      };
+      }
 
 			/**
 			 * Scroll the page by an increment, and check if it's time to stop
 			 */
       var animateScroll = function () {
-        timeLapsed += 16;
-        percentage = (timeLapsed / duration);
-        percentage = (percentage > 1) ? 1 : percentage;
-        position = startLocation + (distance * getEasingPattern(easing, percentage));
+        timeLapsed += 16
+        percentage = (timeLapsed / duration)
+        percentage = (percentage > 1) ? 1 : percentage
+        position = startLocation + (distance * getEasingPattern(easing, percentage))
         if (containerPresent) {
-          container.scrollTop = position;
+          container.scrollTop = position
         } else {
-          window.scrollTo(0, position);
+          window.scrollTo(0, position)
         }
-        stopAnimation();
-      };
+        stopAnimation()
+      }
 
-      callbackBefore(element);
-      var runAnimation = setInterval(animateScroll, 16);
-    }, 0);
-  };
-
+      callbackBefore(element)
+      var runAnimation = setInterval(animateScroll, 16)
+    }, 0)
+  }
 
   // Expose the library in a factory
   //
   module.factory('smoothScroll', function () {
-    return smoothScroll;
-  });
-
+    return smoothScroll
+  })
 
 	/**
 	 * Scrolls the window to this element, optionally validating an expression
@@ -174,29 +171,28 @@
       restrict: 'A',
       scope: {
         callbackBefore: '&',
-        callbackAfter: '&',
+        callbackAfter: '&'
       },
       link: function ($scope, $elem, $attrs) {
         if (typeof $attrs.scrollIf === 'undefined' || $attrs.scrollIf === 'true') {
           setTimeout(function () {
-
             var callbackBefore = function (element) {
               if ($attrs.callbackBefore) {
-                var exprHandler = $scope.callbackBefore({ element: element });
+                var exprHandler = $scope.callbackBefore({ element: element })
                 if (typeof exprHandler === 'function') {
-                  exprHandler(element);
+                  exprHandler(element)
                 }
               }
-            };
+            }
 
             var callbackAfter = function (element) {
               if ($attrs.callbackAfter) {
-                var exprHandler = $scope.callbackAfter({ element: element });
+                var exprHandler = $scope.callbackAfter({ element: element })
                 if (typeof exprHandler === 'function') {
-                  exprHandler(element);
+                  exprHandler(element)
                 }
               }
-            };
+            }
 
             smoothScroll($elem[0], {
               duration: $attrs.duration,
@@ -205,13 +201,12 @@
               callbackBefore: callbackBefore,
               callbackAfter: callbackAfter,
               containerId: $attrs.containerId
-            });
-          }, 0);
+            })
+          }, 0)
         }
       }
-    };
-  }]);
-
+    }
+  }])
 
 	/**
 	 * Scrolls to a specified element ID when this element is clicked
@@ -224,34 +219,34 @@
       restrict: 'A',
       scope: {
         callbackBefore: '&',
-        callbackAfter: '&',
+        callbackAfter: '&'
       },
       link: function ($scope, $elem, $attrs) {
-        var targetElement;
+        var targetElement
 
         $elem.on('click', function (e) {
-          e.preventDefault();
+          e.preventDefault()
 
-          targetElement = document.getElementById($attrs.scrollTo);
-          if (!targetElement) return;
+          targetElement = document.getElementById($attrs.scrollTo)
+          if (!targetElement) return
 
           var callbackBefore = function (element) {
             if ($attrs.callbackBefore) {
-              var exprHandler = $scope.callbackBefore({ element: element });
+              var exprHandler = $scope.callbackBefore({ element: element })
               if (typeof exprHandler === 'function') {
-                exprHandler(element);
+                exprHandler(element)
               }
             }
-          };
+          }
 
           var callbackAfter = function (element) {
             if ($attrs.callbackAfter) {
-              var exprHandler = $scope.callbackAfter({ element: element });
+              var exprHandler = $scope.callbackAfter({ element: element })
               if (typeof exprHandler === 'function') {
-                exprHandler(element);
+                exprHandler(element)
               }
             }
-          };
+          }
 
           smoothScroll(targetElement, {
             duration: $attrs.duration,
@@ -260,12 +255,11 @@
             callbackBefore: callbackBefore,
             callbackAfter: callbackAfter,
             containerId: $attrs.containerId
-          });
+          })
 
-          return false;
-        });
+          return false
+        })
       }
-    };
-  }]);
-
-}());
+    }
+  }])
+}())
--- js/portfolio.js
+++ js/portfolio.js
@@ -1,82 +1,79 @@
 (function (angular) {
-  'use strict';
-  var App = angular.module('portfolio', ['smoothScroll']);
-  var timer;
+  'use strict'
+  var App = angular.module('portfolio', ['smoothScroll'])
+  var timer
 
   App.controller('PortfolioController', function DataController($scope, $http) {
-
-    //accounts
+    // accounts
     $http.get('json/accounts.json')
       .then(function (res) {
-        $scope.accounts = res.data;
-      });
+        $scope.accounts = res.data
+      })
 
-    //timeline
+    // timeline
     $http.get('json/timeline.json')
       .then(function (res) {
-        $scope.timeline = res.data;
-      });
+        $scope.timeline = res.data
+      })
 
-    //interest
+    // interest
     $http.get('json/interest.json')
       .then(function (res) {
-        $scope.interests = res.data;
-      });
+        $scope.interests = res.data
+      })
 
-    //projects
+    // projects
     $http.get('json/projects.json')
       .then(function (res) {
-        $scope.projects = res.data;
-      });
+        $scope.projects = res.data
+      })
 
-    //interest and project effects
-    var prjs, prjContainer, dots;
+    // interest and project effects
+    var prjs, prjContainer, dots
     angular.element(function () {
-      dots = angular.element(document.getElementsByClassName("badge"));
-      prjs = angular.element(document.getElementsByClassName("prj"));
-      prjContainer = angular.element(document.getElementById("projects"));
-      $scope.setBackground(0, false);
-    });
+      dots = angular.element(document.getElementsByClassName('badge'))
+      prjs = angular.element(document.getElementsByClassName('prj'))
+      prjContainer = angular.element(document.getElementById('projects'))
+      $scope.setBackground(0, false)
+    })
 
     $scope.setBackground = function (n, hover = true) {
-      clearTimeout(timer);
-      for (let i = 0; i < dots.length; i = i + 2)
-        set(i, false);
+      clearTimeout(timer)
+      for (let i = 0; i < dots.length; i = i + 2) { set(i, false) }
 
-      set(n * 2, true);
-      $scope.bgInterest = $scope.interests[n].url;
-      if (!hover)
-        $scope.$apply();
+      set(n * 2, true)
+      $scope.bgInterest = $scope.interests[n].url
+      if (!hover) { $scope.$apply() }
 
       timer = setTimeout(function () {
-        $scope.setBackground(n == dots.length / 2 - 1 ? 0 : ++n, false);
-      }, 3000);
-    };
-
-    //mouse enter the project name
-    $scope.enter = function (index) {
-      blurAll();
-      $scope.bgProject = $scope.projects[index].image;
-      prjs[index].style.opacity = 1;
-      prjs[index].style.backgroundColor = "#0B0B0B";
+        $scope.setBackground(n == dots.length / 2 - 1 ? 0 : ++n, false)
+      }, 3000)
     }
 
-    //mouse leave the project name
+    // mouse enter the project name
+    $scope.enter = function (index) {
+      blurAll()
+      $scope.bgProject = $scope.projects[index].image
+      prjs[index].style.opacity = 1
+      prjs[index].style.backgroundColor = '#0B0B0B'
+    }
+
+    // mouse leave the project name
     $scope.leave = function () {
-      $scope.bgProject = "none";
-      blurAll(false);
+      $scope.bgProject = 'none'
+      blurAll(false)
     }
 
     function blurAll(blur = true) {
       for (let i = 0; i < prjs.length; i++) {
-        prjs[i].style.backgroundColor = "transparent";
-        prjs[i].style.opacity = blur ? 0.2 : 1;
+        prjs[i].style.backgroundColor = 'transparent'
+        prjs[i].style.opacity = blur ? 0.2 : 1
       }
     }
 
     function set(i, highlight) {
-      dots[i].style.backgroundColor = highlight ? "white" : "black";
-      dots[i].style.color = highlight ? "black" : "#D4D4D4";
+      dots[i].style.backgroundColor = highlight ? 'white' : 'black'
+      dots[i].style.color = highlight ? 'black' : '#D4D4D4'
     }
-  });
-})(window.angular);
+  })
+})(window.angular)
