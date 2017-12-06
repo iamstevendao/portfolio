9b87107ae1462e0a4fb0b5cafc14c92d88226a80
Steven Dao <iamstevendao@gmail.com>
2017-12-06 17:21:56 +1100

    add draft.js

---
+++ draft.js
@@ -0,0 +1,29 @@
+// Javascript theme
+
+const StevenDao = new Portfolio({
+  Github,   // where you can find a half of my days in
+  Blog,     // some dull stuff written by a dull guy
+  Twitter,  // no thing much but it's where I get updated with cool trends
+  LinkedIn, // another portfolio page, but I don't really use it
+});
+
+const jobs = [
+  {
+    title: '',
+    from: 'xxx',
+    to: 'xxx'
+  },
+  {
+
+  }
+]
+
+const learning = [
+
+]
+
+const projects = [
+  {
+
+  },
+]
